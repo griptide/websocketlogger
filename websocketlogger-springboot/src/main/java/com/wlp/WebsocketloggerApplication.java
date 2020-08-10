14a4ff51a352b7462b422811fea215e2303ca0b5
weilangping <1026749200@qq.com>
2020-08-10 18:23:19 +0800

    初始化

---
+++ websocketlogger-springboot/src/main/java/com/wlp/WebsocketloggerApplication.java
@@ -0,0 +1,13 @@
+package com.wlp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketloggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketloggerApplication.class, args);
+	}
+
+}
---
+++ websocketlogger-springboot/src/test/java/com/wlp/WebsocketloggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wlp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketloggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
